0c2f9988ea7c92463f87668128ace75e49913171
Ayash13 <m.ayashal.f@gmail.com>
2023-12-27 23:38:11 +0700

    Create class InsertViewModel with var insertKontakState and func updateInsertKontakState to insert the data

--- app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,5 +1,24 @@
 package com.example.consumerestapi.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.consumerestapi.data.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent(),
