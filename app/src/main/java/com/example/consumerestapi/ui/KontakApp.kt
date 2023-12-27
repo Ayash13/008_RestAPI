222844f55334995ce0188fa1ecf272f5fcf5b0ff
Ayash13 <m.ayashal.f@gmail.com>
2023-12-28 00:20:39 +0700

    Place PengelolaHalaman inside KontakApp

--- app/src/main/java/com/example/consumerestapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/consumerestapi/ui/KontakApp.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.R
+import com.example.consumerestapi.navigation.PengelolaHalaman
 import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,6 +37,7 @@ fun KontakApp(
                 .fillMaxSize()
                 .padding(it)
         ) {
+            PengelolaHalaman()
         }
     }
 }
