64581c703bd8132e8a85ea5a55f43fe9dbc35f34
Ayash13 <m.ayashal.f@gmail.com>
2023-12-27 23:33:42 +0700

    Create data class InsertUiEvent with every variabel

---
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.consumerestapi.ui.kontak.viewmodel
+
+data class  InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = "",
+)
