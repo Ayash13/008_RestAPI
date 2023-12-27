d17ed41f967bf66974fd33a621cb46be9744a264
Ayash13 <m.ayashal.f@gmail.com>
2023-12-27 23:15:59 +0700

    create function deleteKontak with parameter id

--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/HomeViewModel.kt
@@ -37,4 +37,10 @@ class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel(
             }
         }
     }
+
+    fun deleteKontak(id: Int) {
+        viewModelScope.launch {
+
+        }
+    }
 }
